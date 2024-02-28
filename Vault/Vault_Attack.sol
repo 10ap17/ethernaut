f6b6acc77fc57d43a596fb7c725ba9ccb576713a
10ap17 <150276227+10ap17@users.noreply.github.com>
2024-02-28 22:58:03 +0100

    Add files via upload

---
+++ Vault/Vault_Attack.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Attack{
+    constructor(Vault vault){
+        /*just by typing a command into browser console 'awiat web3.eth.getStorage(address.contract,  1  ) 
+        *                                                                                /             \        
+        *                                                                      address of contract    slot in which the password is located            
+        *
+        *we can see what the password is '0x412076657279207374726f6e67207365637265742070617373776f7264203a29'
+        */
+        bytes32 password= 0x412076657279207374726f6e67207365637265742070617373776f7264203a29;
+        vault.unlock(password);
+    }
+}
